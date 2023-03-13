ce21dc78e451cd69ba195338149573fe5c84bb28
Tyroooo <TestowyMail@git.pl>
2023-03-14 00:13:12 +0100

    Added Dwitter_project_app to settings/Installed_apps | Created models Tweet, Likes and Comments inside models.py  |  registered such models in admin.py

--- Dwitter_project/Dwitter_project/settings.py
+++ Dwitter_project/Dwitter_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Dwitter_project_app',
 ]
 
 MIDDLEWARE = [
--- Dwitter_project/Dwitter_project_app/models.py
+++ Dwitter_project/Dwitter_project_app/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+# Constant that states for user model to use for the Foreign Keys
+USER = settings.AUTH_USER_MODEL
+
+
+class Tweet(models.Model):
+    owner = models.ForeignKey(USER, on_delete=models.CASCADE)
+    body = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Owner: {self.owner}, body: {self.body}"
+
+
+class Likes(models.Model):
+    owner = models.ForeignKey(USER, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    owner = models.ForeignKey(USER, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    body = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)

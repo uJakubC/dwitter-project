f4f55f1bf5dd3d641f7f6f95aece201914b751ac
Tyroooo <TestowyMail@git.pl>
2023-03-18 15:57:07 +0100

    Created TestCase for Tweet model

--- Dwitter_project/Dwitter_project_app/tests.py
+++ Dwitter_project/Dwitter_project_app/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TweetTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create(
+            username="test123",
+            email="test@test.pl",
+            password="secret_password"
+        )
+
+        cls.user2 = User.objects.create(
+            username="user2",
+            email="test@test.pl",
+            password="secret_password"
+        )
+
+    def setUp(self):
+        Tweet.objects.create(owner=self.user, body='Test body only testcase purposes')
+        Tweet.objects.create(owner=self.user2, body='Test body for user2 only testcase purposes')
+
+    def test_tweet_for_user1(self):
+        tweet = Tweet.objects.get(owner=1)
+        self.assertEqual(tweet.body, 'Test body only testcase purposes')
+        self.assertEqual(tweet.o_username, 'test123')
+
+    def test_tweet_o_username_for_user2 (self):
+        tweet = Tweet.objects.get(owner=2)
+        self.assertEqual(tweet.o_username, 'user2')

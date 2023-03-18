e9d22ccf32c3d161587caececddfb20f67b7a557
Tyroooo <TestowyMail@git.pl>
2023-03-18 15:56:40 +0100

    Added lines to define database for test purposes

--- Dwitter_project/Dwitter_project/settings.py
+++ Dwitter_project/Dwitter_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'Dwitter_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -84,7 +85,10 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase'}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

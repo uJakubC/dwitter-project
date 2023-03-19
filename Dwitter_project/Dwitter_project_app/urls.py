01b3e92ae0ca8fcfc5be5460cb6c5d34db693ff0
Tyroooo <TestowyMail@git.pl>
2023-03-19 13:43:38 +0100

    Added url to dweet details page

--- Dwitter_project/Dwitter_project_app/urls.py
+++ Dwitter_project/Dwitter_project_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .class_views import TweetListView
+from .class_views import TweetListView, TweetDetailView
 
 urlpatterns = [
     path("", TweetListView.as_view(), name="tweet-list"),
+    path("dweet/<int:pk>", TweetDetailView.as_view(), name="tweet-detail")
     ]

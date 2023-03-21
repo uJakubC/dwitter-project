9e443349f2c3b2e7ee03afa5b498488e23ab8cd2
Tyroooo <TestowyMail@git.pl>
2023-03-22 00:12:40 +0100

    Added post method inside TweetDetailClass that will perform adding like to dweet

--- Dwitter_project/Dwitter_project_app/class_views.py
+++ Dwitter_project/Dwitter_project_app/class_views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django.views.generic import View
 
-from .forms import AddTweetForm
+from .forms import AddTweetForm, AddLikeForm
 from .models import Tweet, Likes
 
 
@@ -46,11 +46,29 @@ class TweetListView(View):
 
 class TweetDetailView(View):
     def get(self, request: HttpRequest, pk: int) -> HttpResponse:
+        like_form = AddLikeForm()
         try:
             tweet = Tweet.objects.get(pk=pk)
             likes = Likes.objects.filter(tweet_id=pk).count()
-            context = {'tweet': tweet, 'likes': likes}
+            context = {'tweet': tweet, 'likes': likes, 'like_form': like_form}
 
             return render(request=request, template_name="tweet_detail.html", context=context)
         except Tweet.DoesNotExist:
             return HttpResponseNotFound("<h1>Page does not exist</h1> <img src='https://as2.ftcdn.net/v2/jpg/03/39/94/53/1000_F_339945393_2xeDV1SAYvwQTrEQXtuO7lUfpJEOzOVr.jpg' >")
+
+    def post(self, request: HttpRequest, pk: int) -> HttpResponse:
+        tweet = Tweet.objects.get(pk=pk)
+        like_form = AddLikeForm(request.POST)
+        if like_form.is_valid():
+            try:
+                like = like_form.save(commit=False)
+                like.owner = request.user
+                like.tweet = tweet
+                like.save()
+                likes = Likes.objects.filter(tweet_id=pk).count()
+            except IntegrityError:
+                return HttpResponseBadRequest("Bad request")
+
+            context = {'tweet': tweet, 'likes': likes, 'like_form': like_form}
+
+            return render(request=request, template_name="tweet_detail.html", context=context)

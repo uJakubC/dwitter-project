eae926fe4109fac337f3586a813f0a83f2d57ff5
Tyroooo <TestowyMail@git.pl>
2023-03-23 00:10:22 +0100

    Changes for maing page and detail view classes related to comments

--- Dwitter_project/Dwitter_project_app/class_views.py
+++ Dwitter_project/Dwitter_project_app/class_views.py
@@ -4,21 +4,23 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django.views.generic import View
 
-from .forms import AddTweetForm, AddLikeForm
-from .models import Tweet, Likes
+from .forms import AddTweetForm, AddLikeForm, AddCommentForm
+from .models import Tweet, Likes, Comments
 
 
 class TweetListView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         tweets = Tweet.objects.all()
         likes = Likes.objects.values('tweet_id').annotate(total=Count('tweet_id'))
+        comments = Comments.objects.values('tweet_id').annotate(total=Count('tweet_id'))
+        last_five_tweets = tweets.order_by('id')[:5]
         form = AddTweetForm()
 
         if tweets:
-            context = {'tweets': tweets, 'likes': likes, 'form': form}
+            context = {'tweets': tweets, 'likes': likes, 'form': form, 'comments': comments, 'last_five_tweets': last_five_tweets}
             return render(request=request, template_name="tweet_list.html", context=context)
         else:
-            context = {'tweets': tweets, 'empty_query': True, 'form': form}
+            context = {'tweets': tweets, 'empty_query': True, 'form': form, 'comments': comments, 'last_five_tweets': last_five_tweets}
             return render(request=request, template_name="tweet_list.html", context=context)
 
     def post(self, request: HttpRequest) -> HttpResponse:
@@ -31,15 +33,11 @@ class TweetListView(View):
             except IntegrityError:
                 return HttpResponseBadRequest("Bad request")
 
-            tweets = Tweet.objects.all()    # Load all tweets once again
-            likes = Likes.objects.values('tweet_id').annotate(total=Count('tweet_id'))
-            form = AddTweetForm()   # Load clean form
-            context = {'tweets': tweets, 'likes': likes, 'form': form}
-        else:
-            tweets = Tweet.objects.all()  # Load all tweets once again
-            likes = Likes.objects.values('tweet_id').annotate(total=Count('tweet_id'))
-            form = AddTweetForm()  # Load clean form
-            context = {'tweets': tweets, 'likes': likes, 'form': form}
+        tweets = Tweet.objects.all()    # Load all tweets once again
+        likes = Likes.objects.values('tweet_id').annotate(total=Count('tweet_id'))
+        comments = Comments.objects.values('tweet_id').annotate(total=Count('tweet_id'))
+        form = AddTweetForm()   # Load clean form
+        context = {'tweets': tweets, 'likes': likes, 'form': form, 'comments': comments, 'last_five_tweets': last_five_tweets}
 
         return render(request=request, template_name='tweet_list.html', context=context)
 
@@ -47,10 +45,18 @@ class TweetListView(View):
 class TweetDetailView(View):
     def get(self, request: HttpRequest, pk: int) -> HttpResponse:
         like_form = AddLikeForm()
+        comment_form = AddCommentForm()
         try:
             tweet = Tweet.objects.get(pk=pk)
             likes = Likes.objects.filter(tweet_id=pk).count()
-            context = {'tweet': tweet, 'likes': likes, 'like_form': like_form}
+            comments = Comments.objects.filter(tweet_id=pk)
+            context = {
+                'tweet': tweet,
+                'likes': likes,
+                'comments': comments,
+                'like_form': like_form,
+                'comment_form': comment_form
+            }
 
             return render(request=request, template_name="tweet_detail.html", context=context)
         except Tweet.DoesNotExist:
@@ -59,16 +65,37 @@ class TweetDetailView(View):
     def post(self, request: HttpRequest, pk: int) -> HttpResponse:
         tweet = Tweet.objects.get(pk=pk)
         like_form = AddLikeForm(request.POST)
-        if like_form.is_valid():
-            try:
-                like = like_form.save(commit=False)
-                like.owner = request.user
-                like.tweet = tweet
-                like.save()
-                likes = Likes.objects.filter(tweet_id=pk).count()
-            except IntegrityError:
-                return HttpResponseBadRequest("Bad request")
+        comment_form = AddCommentForm(request.POST)
 
-            context = {'tweet': tweet, 'likes': likes, 'like_form': like_form}
+        if 'like_button' in request.POST:
+            if like_form.is_valid():
+                try:
+                    like = like_form.save(commit=False)
+                    like.owner = request.user
+                    like.tweet = tweet
+                    like.save()
+                except IntegrityError:
+                    return HttpResponseBadRequest("Bad request")
 
-            return render(request=request, template_name="tweet_detail.html", context=context)
+        elif 'comment_button' in request.POST:
+            if comment_form.is_valid():
+                try:
+                    comment = comment_form.save(commit=False)
+                    comment.owner = request.user
+                    comment.tweet = tweet
+                    comment.save()
+                except IntegrityError:
+                    return HttpResponseBadRequest("Bad request")
+
+        likes = Likes.objects.filter(tweet_id=pk).count()
+        comments = Comments.objects.filter(tweet_id=pk)
+        comment_form = AddCommentForm()
+        context = {
+            'tweet': tweet,
+            'likes': likes,
+            'comments': comments,
+            'like_form': like_form,
+            'comment_form': comment_form
+        }
+
+        return render(request=request, template_name="tweet_detail.html", context=context)

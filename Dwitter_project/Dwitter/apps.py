b7cafedb7c671fcfd0c5f0306a085eedbab2a012
Tyroooo <TestowyMail@git.pl>
2023-03-28 00:18:27 +0200

    A little refactor of project structure. It will now have 2 apps which were renamed and we shared responsibilities between them

--- Dwitter_project/Dwitter/apps.py
+++ Dwitter_project/Dwitter/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DwitterProjectAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Dwitter_project_app'
+    name = 'Dwitter'
